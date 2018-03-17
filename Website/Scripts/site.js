3beb340f4b8ef365cf8bbdf52fef2acdee2e00aa
Esha <esha.s>
2018-03-16 21:10:28 -0400

    adding donor pages
    donor pages

---
+++ Website/Scripts/site.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  // TODO: Replace with your project's customized code snippet
+  var config = {
+    apiKey: "AIzaSyCrzMD6oJXUi46tb_dt371H4HcvEOR5p2I",
+    authDomain: "gwc-homelessness.firebaseapp.com",
+    databaseURL: "https://gwc-homelessness.firebaseio.com",
+    storageBucket: "gwc-homelessness.appspot.com",
+    messagingSenderId: "973683998221",
+  };
+  firebase.initializeApp(config);
