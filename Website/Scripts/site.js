27ba8ce968c06f5ba5a59f7df1ff86c939274cd7
idispose <idispose@outlook.com>
2018-03-16 21:58:44 -0400

    added site.js to donor page

--- Website/Scripts/site.js
+++ Website/Scripts/site.js
@@ -2,6 +2,7 @@
   // Initialize Firebase
   // TODO: Replace with your project's customized code snippet
   var config = {
+    projectId: "gwc-homelessness",
     apiKey: "AIzaSyCrzMD6oJXUi46tb_dt371H4HcvEOR5p2I",
     authDomain: "gwc-homelessness.firebaseapp.com",
     databaseURL: "https://gwc-homelessness.firebaseio.com",
