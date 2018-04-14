088ad021600d10ac59f758d29fa48c9ac375faf4
Sabrina <33787198+Woof147@users.noreply.github.com>
2018-04-14 11:09:59 -0400

    added zipcode to table

--- Website/scripts/jobseeker.js
+++ Website/scripts/jobseeker.js
@@ -21,7 +21,7 @@ db.collection("jobs").where(condition, "==", value).get()
     jobs.forEach(function(job){
         //alert(job.data().businessName);
         var jobData=job.data();
-        tbody.append("<tr><td>"+jobData.businessName+"</td><td>"+jobData.cityName+"</td><td>"+jobData.description+"</td><td>"+jobData.hourlyRate+"</td><td>"+jobData.startDate+"</td><td>"+jobData.endDate+"</td><td>"+jobData.jobName+"</td></tr>")
+        tbody.append("<tr><td>"+jobData.businessName+"</td><td>"+jobData.cityName+"</td><td"+jobData.zipcode+"></td><td>"+jobData.description+"</td><td>"+jobData.hourlyRate+"</td><td>"+jobData.startDate+"</td><td>"+jobData.endDate+"</td><td>"+jobData.jobName+"</td></tr>")
         
     });
 
